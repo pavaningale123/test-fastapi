247b886a3d0ed3276ce71721d865f870921c4994
Pavan Ingale <pavaningale7849@gmail.com>
2026-02-11 19:01:39 +0530

    new curd operation

--- model_val.py
+++ model_val.py
@@ -4,4 +4,4 @@ class Employee(BaseModel):
     id: int = Field(..., gt=0, description="The unique identifier for the employee")
     name: str   = Field(..., min_length=3, max_length=30, description="The name of the employee")
     department: str = Field(..., min_length=3, max_length=30, description="The department of the employee")
-    age: int = Field(..., gt=0, lt=50, description="The age of the employee")
+    age: int = Field(..., gt=0, lt=50, description="The age of the employee") 
